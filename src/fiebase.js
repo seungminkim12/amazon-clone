5774e26ab6b92a0b3aa1ff45a27981ba3016a730
SeungminKim <ksm0057@naver.com>
2020-09-09 15:24:52 +0900

    setting

---
+++ src/fiebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCx8Mo8WRa7GOsonRyqa4t4jTYg-AsiNs8",
+  authDomain: "clone-d9cbf.firebaseapp.com",
+  databaseURL: "https://clone-d9cbf.firebaseio.com",
+  projectId: "clone-d9cbf",
+  storageBucket: "clone-d9cbf.appspot.com",
+  messagingSenderId: "184447847740",
+  appId: "1:184447847740:web:c40cd5968fb1d75b312239",
+  measurementId: "G-V8KL3WMF5X",
+};
