a993c57e9d4a6964feb9e7c67bc7ded123a404c5
SeungminKim <ksm0057@naver.com>
2020-09-09 17:51:55 +0900

    css1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
 import "./App.css";
+import Header from "./Header.js";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {/* Header */}
+      <Header />
+      {/* Home */}
+    </div>
+  );
 }
 
 export default App;
