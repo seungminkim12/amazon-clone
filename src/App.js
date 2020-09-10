131b22ffc45b94c399f6b32fba7808b8ec135cae
SeungminKim <ksm0057@naver.com>
2020-09-10 17:46:33 +0900

    add backgroundImage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import Header from "./Header.js";
+import Home from "./Home.js";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       {/* Header */}
       <Header />
       {/* Home */}
+      <Home />
     </div>
   );
 }
