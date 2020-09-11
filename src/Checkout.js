5ac7d5523169dd86ce900dc0240851ced57d149c
SeungminKim <ksm0057@naver.com>
2020-09-11 23:55:50 +0900

    css4

--- src/Checkout.js
+++ src/Checkout.js
@@ -7,7 +7,7 @@ function Checkout() {
       <div className="checkout__left">
         <img
           className="checkout__ad"
-          src="https://imaages-na.ssl-imaages-amazon.com/images/G/02/UK_CCMP/TM/0CC_Amazon1._CB423492668_.jpg"
+          src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
         />
       </div>
     </div>
