52ba1a8bd365bd21dabdf18a26f692bff2b5f14b
SeungminKim <ksm0057@naver.com>
2020-09-20 20:34:11 +0900

    fix and add backEndThing

--- src/Payment.js
+++ src/Payment.js
@@ -7,6 +7,7 @@ import { Link, useHistory } from "react-router-dom";
 import CurrencyFormat from "react-currency-format";
 import { getBasketTotal } from "./reducer";
 import axios from "./axios";
+import { db } from "./firebase";
 
 function Payment() {
   const [{ basket, user }, dispatch] = useStateValue();
@@ -48,6 +49,17 @@ function Payment() {
       })
       .then(({ paymentIntent }) => {
         // paymentIntent = payment confirmation
+
+        db.collection("users")
+          .doc(user.uid)
+          .collection("orders")
+          .doc(paymentIntent.id)
+          .set({
+            basket: basket,
+            amount: paymentIntent.amount,
+            created: paymentIntent.created,
+          });
+
         setSucceeded(true);
         setError(null);
         setProcessing(false);
